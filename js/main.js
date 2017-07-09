cc5879fe73aae877fe482c7b07fb0ddbeea445d7
moradsimplon <morad.bzioui@laposte.net>
2017-07-09 17:34:53 +0200

    add style in js

--- js/main.js
+++ js/main.js
@@ -1,66 +1,122 @@
 
-  var choixuser="";
-function choix(x){
-choixuser=x;
-//document.getElementById('demo').innerHTML =  choixuser;
-if (choixuser=="ciseaux"){
-document.getElementById("feuille").style.display = "none";
-document.getElementById("pierre").style.display = "none";
-}
-else if (choixuser=="feuille") {
-  document.getElementById("ciseaux").style.display = "none";
+var a;
+var choixuser;
+var choiOrdi;
+var scoreuser=0;
+var scoreordi=0;
+
+
+
+function hideOther() {
+  if (choixuser=="ciseaux"){
+  document.getElementById("feuille").style.display = "none";
   document.getElementById("pierre").style.display = "none";
 
+  }
+  else if (choixuser=="feuille") {
+    document.getElementById("ciseaux").style.display = "none";
+    document.getElementById("pierre").style.display = "none";
+
+
+  }
+  else {
+    document.getElementById("ciseaux").style.display = "none";
+    document.getElementById("feuille").style.display = "none";
+
+  }
 }
-else {
-  document.getElementById("ciseaux").style.display = "none";
-  document.getElementById("feuille").style.display = "none";
+
+function scoregeneral() {
+  if (scoreuser==="3"){
+    document.getElementById('scoregen').src="img/win.gif";
+
+  }
+  else if (scoreordi==="3") {
+    document.getElementById('scoregen').src="img/lose.gif";
+
+  }
 }
-}
+function game(a) {
 
 
 
-function game() {
+  choixuser=a;
+  hideOther();
 
 
-var a=prompt("Jouer");
 var b = Math.floor((Math.random() * 3) + 1);
+console.log(b);
 
 
 if(b==1){
-  b= "papier";
+  document.getElementById('ordi').src="img/feuille.png";
+  document.getElementById("ordi").style.display = "block";
+  choixOrdi = "feuille";
 } else if (b==2){
-   b="pierre";
+    document.getElementById('ordi').src="img/pierre.png";
+      document.getElementById("ordi").style.display = "block";
+    choixOrdi="pierre";
 } else {
-  b="ciseaux";
+    document.getElementById('ordi').src="img/ciseaux.png";
+      document.getElementById("ordi").style.display = "block";
+    choixOrdi="ciseaux";
 }
+console.log(choixOrdi);
 
 
-document.getElementById('id').innerHTML="l'ordinateur joue" + " " + b
-
-
-if(a==b){
+if(choixuser==choixOrdi){
   document.getElementById('image').src = "img/egalite.gif";
+  document.getElementById("image").style.display = "block";
 }
-else if(a==="pierre" && b==="papier"){
+else if(choixuser=="pierre" && choixOrdi=="feuille"){
   document.getElementById('image').src = "img/perdu.gif";
+    document.getElementById("image").style.display = "block";
+  scoreordi++;
 }
-else if(a==="ciseaux" && b==="papier"){
+
+else if(choixuser=="pierre" && choixOrdi=="ciseaux"){
   document.getElementById('image').src = "img/gagne.gif";
+    document.getElementById("image").style.display = "block";
+  scoreuser++;
 }
-else if(a==="papier" && b==="pierre"){
+
+ else if(choixuser=="feuille" && choixOrdi=="pierre"){
   document.getElementById('image').src = "img/gagne.gif";
+    document.getElementById("image").style.display = "block";
+  scoreuser++;
 }
-else if(a==="papier" && b==="ciseaux"){
+else if(choixuser=="feuille" && choixOrdi=="ciseaux"){
   document.getElementById('image').src = "img/perdu.gif";
+    document.getElementById("image").style.display = "block";
+  scoreordi++;
 }
-else if(a==="ciseaux" && b==="pierre"){
+
+
+else if(choixuser=="ciseaux" && choixOrdi=="pierre"){
   document.getElementById('image').src = "img/perdu.gif";
+    document.getElementById("image").style.display = "block";
+  scoreordi++;
 }
-else if(a==="ciseaux" && b==="papier"){
+else if(choixuser=="ciseaux" && choixOrdi=="feuille"){
   document.getElementById('image').src = "img/gagne.gif";
+    document.getElementById("image").style.display = "block";
+  scoreuser++;
 }
-else if (a === "" || a !== "pierre" || a !== "papier" || a !== "ciseaux") {
+
+else if (choixuser == "" || choixuser !== "pierre" || choixuser !== "feuille" || a !== "ciseaux") {
 alert("Entre une réponse, correct");
-}
+ }
+document.getElementById("score").innerHTML= scoreuser +" " + "vs" + " "  + scoreordi ;
+setTimeout(function () {
+  // mettre les instruction
+  document.getElementById("feuille").style.display = "block";
+  document.getElementById("pierre").style.display = "block";
+  document.getElementById("ciseaux").style.display = "block";
+  document.getElementById("ordi").style.display = "none";
+  document.getElementById("image").style.display = "none";
+
+
+}, 3000);
+scoregeneral();
+
 }
